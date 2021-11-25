d3b7c79e2cdde9bdcd7e5c2d719787593c8d5b3b
LuisMlopez <lucho-1100@hotmail.com>
2021-11-25 10:19:07 +0000

    Create Customer model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+
+    photo = models.ImageField(null=True, upload_to='customer_photos')
+
+    birthday = models.DateField(null=True)
+    email = models.EmailField(null=True)
+    phone = models.CharField(null=True, max_length=20)
+
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='creator')
+    updated_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='editor')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
