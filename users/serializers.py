ccd70f0f41b35328a62e256a57a02c0f238184b3
LuisMlopez <lucho-1100@hotmail.com>
2021-11-26 08:15:22 +0000

    Add token to users list and hide password field

--- users/serializers.py
+++ users/serializers.py
@@ -3,10 +3,14 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(source='auth_token.key', read_only=True)
     class Meta:
         model = User
         fields = [
             'id', 'username', 'first_name', 'last_name', 'email', 'password', 'is_active', 'is_staff',
-            'date_joined'
+            'date_joined', 'token'
         ]
-        read_only_fields = ['date_joined']
+        read_only_fields = ['date_joined', 'token']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
