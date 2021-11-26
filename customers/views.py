6a2c34bf037ca641c616221a488793052924c4b4
LuisMlopez <lucho-1100@hotmail.com>
2021-11-26 11:32:08 +0000

    Document services

--- customers/views.py
+++ customers/views.py
@@ -7,6 +7,56 @@ from customers.models import Customer
 class CustomerViewSet(viewsets.ModelViewSet):
     """
         A ViewSet for viewing, creating and editing customers
+
+        list:
+
+        >
+        > **Description**
+        >
+        > Allow list all customers
+        >
+
+        create:
+
+        >
+        > **Description**
+        >
+        > Allow create a customer.
+        >
+
+        read:
+
+        >
+        > **Description**
+        >
+        > Read a customer information using the user ID.
+        >
+
+        update:
+
+        >
+        > **Description**
+        >
+        > Update a customer. The name and surname fields are always required. 
+        > Use the partial_update method to update a customer without sendig required fields.
+        >
+
+        partial_update:
+
+        >
+        > **Description**
+        >
+        > Allow update a customer information without sendig the required fields.
+        >
+
+        delete:
+
+        >
+        > **Description**
+        >
+        > Allow remove a customer.
+        >
+        
     """
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
--- users/views.py
+++ users/views.py
@@ -7,7 +7,59 @@ from users.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-        A ViewSet for viewing, creating and editing users
+        A ViewSet for viewing, creating and editing users.
+        Only a staff user can use this services.
+
+        list:
+
+        >
+        > **Description**
+        >
+        > Allow list all users
+        >
+
+        create:
+
+        >
+        > **Description**
+        >
+        > Allow create a new user.
+        >
+
+        read:
+
+        >
+        > **Description**
+        >
+        > Read an user information using the user ID.
+        >
+
+        update:
+
+        >
+        > **Description**
+        >
+        > Update an user. The username and password fields are always required. 
+        > Use the partial_update method to update an user without sendig required fields.
+        >
+
+        partial_update:
+
+        >
+        > **Description**
+        >
+        > Allow update an user information without sendig the required fields.
+        > Use this service to change the user admin status.
+        >
+
+        delete:
+
+        >
+        > **Description**
+        >
+        > Allow remove an user.
+        >
+        
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
